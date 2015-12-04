ff408232f3576d9c5902f8c953b6c0d25afc05b6
Viktor Plane <viktorplane@captiveportal-5-200.local.su.se>
2015-12-04 12:26:42 +0100

    Initial Commit

---
+++ PROG3_INLUPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PROG3_INLUPP
+//
+//  Created by Viktor Plane on 04/12/15.
+//  Copyright © 2015 Viktor Plane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
